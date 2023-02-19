f7d16baa71bed55487088fbfac6102fe3ef4180f
jiuuu26 <wldn4036@naver.com>
2023-02-19 13:35:14 +0900

    django course 45 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -9,7 +9,6 @@ app_name = "accountapp"
 
 urlpatterns = [
      #함수형 view
-     path('hello_world/', hello_world, name='hello_world'),
 
      path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
      path('logout/', LogoutView.as_view(), name='logout'),
