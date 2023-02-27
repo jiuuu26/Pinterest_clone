1e5f47f047506ef40d20a894e449d9453f2b5674
jiuuu26 <wldn4036@naver.com>
2023-02-28 01:03:50 +0900

    django course 60 commit

--- pragmatic/settings/base.py
+++ pragmatic/settings/base.py
@@ -16,30 +16,10 @@ import environ
 import os
 
 from django.urls import reverse_lazy
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# Take environment variables from .env file
-environ.Env.read_env(
-   # env_file=os.path.join(BASE_DIR,'.env')
-)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,15 +72,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pragmatic.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
